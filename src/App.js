10db59f59c42c9fd98c0972c36cf8471d6a9a20f
m-ghiringhelli <mghiring@gmail.com>
2022-03-21 12:31:04 -0700

    added browser router and files to route to

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import Main from './views/Main/Main';
+import Books from './views/Books/Books';
+import Characters from './views/Characters/Characters';
+import Films from './views/Films/Films';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <Switch>
+          <Route exact path='/'>
+            <Main />
+          </Route>
+          <Route path='/books'>
+            <Books />
+          </Route>
+          <Route path='/characters'>
+            <Characters />
+          </Route>
+          <Route path='/films'>
+            <Films />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
