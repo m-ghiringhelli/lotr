32604b08efe27754720e953295ffaadc2e942ca3
m-ghiringhelli <mghiring@gmail.com>
2022-03-21 14:18:33 -0700

    display movie titles on films page

--- src/views/Films/Films.js
+++ src/views/Films/Films.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchFilms } from '../../services/films';
 
 export default function Films() {
+  const [films, setFilms] = useState([]);
+  const [errorMessage, setErrorMessage] = '';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resp = await fetchFilms();
+        setFilms(resp);
+      } catch (e) {
+        setErrorMessage('dangit broke');
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div>Films</div>
+    <div>
+      <p>{errorMessage}</p>
+      <div>
+        {films.map((film) => (
+          <p key={film.id}>{film.title}</p>
+        ))}
+      </div>
+    </div>
   );
 }
